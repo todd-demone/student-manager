eba8c96be0052a4284e64d5b44e002cc5c6cfd1f
Todd Demone <tdemone1@gmail.com>
2025-03-01 08:28:23 -0500

    Start StudentService.java

---
+++ src/main/java/com/todddemone/studentmanager/StudentService.java
@@ -0,0 +1,16 @@
+package com.todddemone.studentmanager;
+
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    // createStudent
+    // removeStudent
+    // updateStudent
+    // getStudentDetails
+    // listStudents
+    // getCoursesOfStudent
+}
